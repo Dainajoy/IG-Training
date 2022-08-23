31365af87425a3633d3aaae800ef9df9236f0e4a
Dainajoy <107020707+Dainajoy@users.noreply.github.com>
2022-08-23 15:46:21 +0530

    Add files via upload

---
+++ SpringAutowireSetterInjectionWithByNameAndByType/SpringWithSetterInjection/src/com/spring/autowire/Heart.java
@@ -0,0 +1,9 @@
+package com.spring.autowire;
+
+public class Heart {
+
+    public void pump() {
+        System.out.println("Your heart is pumping");
+        System.out.println("You are alive");
+    }
+}
