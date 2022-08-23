8424293b7781b8b2be30928455ddbf6d0895991a
Dainajoy <107020707+Dainajoy@users.noreply.github.com>
2022-08-23 16:00:58 +0530

    Delete SpringAutowireSetterInjectionWithByNameAndByType/SpringWithSetterInjection directory

--- SpringAutowireSetterInjectionWithByNameAndByType/SpringWithSetterInjection/src/com/spring/autowire/Heart.java
+++
@@ -1,9 +0,0 @@
-package com.spring.autowire;
-
-public class Heart {
-
-    public void pump() {
-        System.out.println("Your heart is pumping");
-        System.out.println("You are alive");
-    }
-}
